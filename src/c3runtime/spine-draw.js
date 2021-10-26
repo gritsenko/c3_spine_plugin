33519df26131fcf9c9579275775ca6e10db0a6cb
MikalDev <keg@kindeyegames.com>
2021-10-26 10:44:19 -0700

    fix (worker): add patch from spine-ts runtime fix for worker mode
    spine-ts fix:
    
    https://github.com/EsotericSoftware/spine-runtimes/commit/6314eaf9083fc32681728c16d92eaba6a04face5

--- src/c3runtime/spine-draw.js
+++ src/c3runtime/spine-draw.js
@@ -346,7 +346,7 @@ class SpineBatch {
 // @ts-ignore
 if (!globalThis.spineBatcher)
 {
-    console.log('[Spine] SpineBatcher init, 1.50.1');
+    console.log('[Spine] SpineBatcher init, 1.50.2');
     // @ts-ignore
     globalThis.spineBatcher = new SpineBatch();
 }
